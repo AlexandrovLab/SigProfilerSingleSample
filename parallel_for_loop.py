9c9b7ca510e6daf2e7252cc947ff7da5c2053941
reddyashrith <43897069+reddyashrith@users.noreply.github.com>
2018-12-14 15:06:32 -0800

    Exactly matches MATLAB output

--- parallel_for_loop.py
+++ parallel_for_loop.py
@@ -37,13 +37,14 @@ def parallel_for_loop(iSample, inputSamples, allGenomeSignatures, allExomeSignat
     
     strandBias = inputSamples['strandBias']
     
+    #print(sum(exposures))
     if ( useRules == 1 ):
         exposures = check_signature_rules(exposures,
                                           sigNames,
                                           iSample,
                                           seqType,
                                           totalMutations,
-                                          strandBias,
+                                          #strandBias,
                                           'input/C_to_A_p.txt',
                                           'input/C_to_A_d.txt',
                                           'input/C_to_G_p.txt',
@@ -60,12 +61,27 @@ def parallel_for_loop(iSample, inputSamples, allGenomeSignatures, allExomeSignat
                                           'input/T_to_G_d.txt',
                                           'input/20181108_Signature_Rules.xml',
                                           'input/Signature_Rules_Schema.xsd')
-            
+    
+    #print('signaturesInsample')
+    #print(exposures)
+    #print('signatures')
+    #print(sigNames)
+    #print('sampleID')
+    #print(iSample)
+    #print('seqType')
+    #print(seqType)
+    #print('totalMutations')
+    #print(totalMutations)
+    #print('strandBias')
+    #print(strandBias)
+       
     #Remove all signatures in the sample: one by one
     #Add signatures that are allowed everywhere
     #if ( len(idsToAdd) > 0 ): #BEWARE modification to code in a few more ifs down as well
+    
     if (idsToAdd != False):
         exposures[idsToAdd] = 1 
+    #print(exposures)
     
     #Add connected signatures
     if (len(connected) > 0):
@@ -73,24 +89,29 @@ def parallel_for_loop(iSample, inputSamples, allGenomeSignatures, allExomeSignat
             if ( sum(exposures[connected[iConnect]]) > 0 ):
                 exposures[connected[iConnect]] = 1
     
-    [exposuresOutput, accr, kl_div, frob_rel_div, norm_one_dif] = remove_all_single_signatures(exposures, allSignatures, genome, saOptions, sigNames)
-       
+    #[exposuresOutput, accr, kl_div, frob_rel_div, norm_one_dif] = remove_all_single_signatures(exposures, allSignatures, genome, saOptions, sigNames)
+    #print(exposures)
+    [exposures, accr, kl_div, frob_rel_div, norm_one_dif] = remove_all_single_signatures(exposures, allSignatures, genome, saOptions, sigNames)
+    #print(exposures)
+    #print(exposures)   
     #Add all remaining signatures to the sample: one by one
     #Add signatures that are allowed everywhere
     if (idsToAdd != False):
         for i in idsToAdd:
             exposures[i] = 1
-            
+    #print(exposures)        
     #Add connected signatures
     if (len(connected) > 0):
         for iConnect in range(len(connected)):
             if ( sum(exposures[connected[iConnect]]) > 0 ):
                 exposures[connected[iConnect]] = 1
-                
+    #print(exposures)      
+    #print(sum(exposures)) #BEWARE DIFFERENT OUTPUT AS COMPARED TO MATLAB      
     exposures = add_all_single_signatures(exposures, allSignatures, genome, saOptions, sigNames)
-        
+    #print(sum(exposures))    
     #Add signatures that are allowed everywhere
     #Add signatures that are allowed everywhere
+    #print(exposures)
     if (idsToAdd != False):
         exposures[idsToAdd] = 1
 
@@ -99,10 +120,15 @@ def parallel_for_loop(iSample, inputSamples, allGenomeSignatures, allExomeSignat
         for iConnect in range(len(connected)):
             if ( sum(exposures[connected[iConnect]]) > 0 ):
                 exposures[connected[iConnect]] = 1
-        
+    
+    #print("1")
+    #print(exposures)
+    #print(sum(exposures))     
     [exposures, accr_org[iSample], kl_div_org[iSample], frob_rel_div_org[iSample], norm_one_dif_org[iSample]] = eval_single_sample(exposures, allSignatures, genome, saOptions)
+    #print(sum(exposures))
+    #print(exposures)
     exposuresNew[:, iSample] = exposures
 
     #Dispay summary output data
-    out_str = ['Sample #' + str(iSample) + ': ' + str(inputSamples['cancerType'][iSample]) + ' ' + str(inputSamples['sampleNames'][iSample]) + ' with ' + str(sum(exposuresNew[:, iSample]>0)) + ' signatures and an accuracy of ' + str(accr_org[iSample])] #BEWARE ,'%.2f' removed
+    out_str = ['Sample #' + str(iSample+1) + ': ' + str(inputSamples['cancerType'][iSample][0]) + ' ' + str(inputSamples['sampleNames'][iSample][0]) + ' with ' + str(sum(exposuresNew[:, iSample]>0)) + ' signatures and an accuracy of ' + str(round(accr_org[iSample][0],2))] #BEWARE ,'%.2f' removed
     return(out_str)               
